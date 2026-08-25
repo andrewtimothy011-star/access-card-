8b1f3579193b5eff84c939b1d2957f4e771c7adc
Andrew <andrewtimothy011@gmail.com>
2026-08-25 14:09:07 -0700

    commit

---
+++ accesscard.py
@@ -0,0 +1,6 @@
+stored_name = ("Tony", "Femi", "Titi", "Goodness", "Samuel", "Ayoife", "Ayo", "Gift", "Micheal", "Daniel")
+name = input("Enter your name: ")
+if name in stored_name:
+    print("Access Card granted")
+else:
+    print("Access card denied")
